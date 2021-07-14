8bb482188095291fd0c3775e9449377a8fc5602f
Pedro Palmeira <34588988+Peticali@users.noreply.github.com>
2021-07-14 03:10:43 -0300

    Add files via upload

---
+++ fluentTkinter.py
@@ -0,0 +1,43 @@
+
+from tkinter import *
+import ctypes
+from TkinterNEW import new
+
+root = Tk()
+root.config(bg='green')
+
+root.wm_attributes("-transparent", 'green')
+root.geometry('500x400')
+root.update()
+
+global HWND
+HWND = ctypes.windll.user32.GetForegroundWindow()
+
+new.BlurWorkAround(root,HWND)
+
+new.NewPanel()
+
+l = new.NewLabel(text='New Widgets')
+l.place(relx=.4,y=1)
+
+
+#NewEntry
+e = new.NewEntry(width=9)
+e[1].insert(0,'Entry')
+e[0].place(relx=.4,y=50)
+
+
+#NewButton
+b = new.NewButton(text='Button')
+b[0].place(relx=.4,y=100)
+
+
+#NewDropDown
+choosed = StringVar() 
+choosed.set('1')
+objs = ['1','2','3'] #list 
+b = new.NewDropDown(objs=objs,variable=choosed)
+b[0].place(relx=.4,y=150)
+
+
+root.mainloop()
